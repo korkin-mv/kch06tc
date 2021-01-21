92c0cbf00f33e47ce55f3fe4673345fc5698d7a8
Mihail Korkin <korkin_mv@protonmail.ch>
2021-01-22 00:49:05 +0500

    [Firmware] Delete C files

--- src/text.c
+++
@@ -1,8 +0,0 @@
-#include "defines.h"
-#include "text.h"
-
-const char text_newline[] PROGMEM = "\r\n";
-const char text_prompt[] PROGMEM = VT_PROMPT;
-const char text_delete_in_line[] PROGMEM = "\e[K";
-const char text_backspace[] PROGMEM = "\b \b";
-const char text_not_found[] PROGMEM = ": command not found.\r\n";
