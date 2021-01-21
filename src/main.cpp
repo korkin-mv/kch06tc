da1aaf9966812c965587ac4793d5269cfbbf82bd
Mihail Korkin <korkin_mv@protonmail.ch>
2021-01-22 01:45:33 +0500

    [Firmware] Add support YCMOS

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,34 @@
+#include <avr/interrupt.h>
+
+#include <ycmos/io/user/VT.hpp>
+
+#include "UART.hpp"
+
+using namespace ycmos;
+using namespace ycmos::containers;
+using namespace ycmos::io::user;
+
+void operator delete(void *, unsigned int)
+{
+
+}
+
+char buf[30];
+
+UART uart;
+String s(buf, sizeof(buf));
+VT vt(&uart);
+Pt pt;
+
 [[gnu::OS_main]] int main()
 {
-    for (;;);
+    UART::init<9600>();
+    PT_INIT(&pt);
+    sei();
+    for (;;)
+    {
+        while(PT_SCHEDULE(vt.gets(&pt, s)));
+        while(PT_SCHEDULE(vt.puts(&pt, s)));
+        while(PT_SCHEDULE(vt.puts(&pt, VT::newline)));
+    }
 }
